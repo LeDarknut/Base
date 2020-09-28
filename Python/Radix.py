5ead04de6df402d3c2e18579fe7aaa5f8a94683d
LeDarknut <darkyledarknut@gmail.com>
2020-09-28 19:56:45 +0200

    Update Radix.py

--- Python/Radix.py
+++ Python/Radix.py
@@ -1,5 +1,5 @@
-def convert(inputValue:str, inputRadix:str, outputRadix:str) :
-	
+#============================================#
+
 	#---------------Usage----------------#
 	
 	# This fonction converts a string which represents an integer value encoded with a radix to another radix
@@ -7,66 +7,56 @@ def convert(inputValue:str, inputRadix:str, outputRadix:str) :
 	#--------------Inputs----------------#
 	
 	#inputValue:str		-> The string which represents a integer value
-	#inputRadix:str		-> The input radix declaration which contains all the digits of the radix in order (decimal radix = "0123456789", hexadecimal radix = "0123456789ABCDEF")
+	#inputRadix:str		-> The input radix declaration which contains all the digits of the radix in order (binary radix = "01", decimal radix = "0123456789", hexadecimal radix = "0123456789ABCDEF")
 	#outputRadix:str	-> The output radix declaration which works just as the input radix
 	
 	#-------------Outputs----------------#
 	
 	#outputValue:str	-> The input value encoded with the output radix
-
-	#------------Declarations------------#
 	
+#============================================#
+	
+def convert(inputValue:str, inputRadix:str, outputRadix:str) :
 	intValue = 0
 	outputValue = ""
-	
-	#-------------Algorithm--------------#
-	
 	inputValue = inputValue[::-1]
 	for i in range(len(inputValue)) :
 		intValue += inputRadix.index(inputValue[i]) * (len(inputRadix) ** i)
 	while intValue > 0 :
 		outputValue = outputValue + str(intValue % len(outputRadix))
 		intValue = intValue // len(outputRadix)
-		
-	#---------------Return---------------#
-	
 	return outputValue[::-1]
+
+
 
 #============================================#
 
-def decode(inputValue:str, inputRadix:str) :
-	
 	#---------------Usage----------------#
 	
 	#This fonction decodes a string which represents an integer value encoded with a radix
 	
 	#--------------Inputs----------------#
 	
-	#inputValue:str		-> The string which represents a integer value
-	#inputRadix:str		-> The input radix declaration which contains all the digits of the radix in order (decimal radix = "0123456789", hexadecimal radix = "0123456789ABCDEF")
+	#inputValue:str		-> The string which represents an integer value encoded with a radix
+	#inputRadix:str		-> The input radix declaration which contains all the digits of the radix in order (binary radix = "01", decimal radix = "0123456789", hexadecimal radix = "0123456789ABCDEF")
 	
 	#-------------Outputs----------------#
 	
 	#outputValue:str	-> The input value decoded
 
-	#------------Declarations------------#
-	
+#============================================#
+
+def decode(inputValue:str, inputRadix:str) :
 	outputValue = 0
-	
-	#-------------Algorithm--------------#
-	
 	inputValue = inputValue[::-1]
 	for i in range(len(inputValue)) :
 		outputValue += inputRadix.index(inputValue[i]) * (len(inputRadix) ** i)
-		
-	#---------------Return---------------#
-	
 	return outputValue
-	
+
+
+
 #============================================#
 
-def encode(inputValue:int, outputRadix:str) :
-	
 	#---------------Usage----------------#
 	
 	#This fonction encodes an integer value to a radix
@@ -74,22 +64,17 @@ def encode(inputValue:int, outputRadix:str) :
 	#--------------Inputs----------------#
 	
 	#inputValue:int		-> The integer value
-	#outputRadix:str	-> The output radix declaration which contains all the digits of the radix in order (decimal radix = "0123456789", hexadecimal radix = "0123456789ABCDEF")
+	#outputRadix:str	-> The output radix declaration which contains all the digits of the radix in order (binary radix = "01", decimal radix = "0123456789", hexadecimal radix = "0123456789ABCDEF")
 	
 	#-------------Outputs----------------#
 	
 	#outputValue:str	-> The input value encoded with the output radix
 
-	#------------Declarations------------#
-	
+#============================================#
+
+def encode(inputValue:int, outputRadix:str) :
 	outputValue = ""
-	
-	#-------------Algorithm--------------#
-	
 	while inputValue > 0 :
 		outputValue = outputValue + str(inputValue % len(outputRadix))
 		inputValue = inputValue // len(outputRadix)
-		
-	#---------------Return---------------#
-	
 	return outputValue[::-1]
